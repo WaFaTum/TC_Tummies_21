590eaf69e35e769e71ef700f007b40d11ece425f
WaFaTum <86512468+WaFaTum@users.noreply.github.com>
2021-08-06 23:01:53 +0100

    Delete New Tech_21 directory

--- New Tech_21/include/I2CSensor.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-class I2CSensor{
-    protected: int device Addr;
-    public: virtual bool init ()=0;
-    virtual int sigleShotMeasurement()=0;
-    virtual int continuousMeasurement()=0;
-}
